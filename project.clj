aecb0e506cdc19142bf4f537919e2c5777abf2e7
nakort <nakort@gmail.com>
2012-10-19 11:40:20 -0430

    Simple API that does not use permanent storage.

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject linkshortener "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Link shortener"
+  :main linkshortener.core
   :dependencies [[org.clojure/clojure "1.4.0" ]
                  [ring/ring-json "0.1.2" ]
                  [org.clojure/data.json "0.2.0"]
---
+++ src/linkshortener/core.clj
@@ -0,0 +1,36 @@
+(ns linkshortener.core 
+  (:gen-class :main true)
+  (:require [clojure.data.json :as json]
+            [compojure.core :refer :all]
+            [ring.middleware.json :refer :all]
+            [ring.adapter.jetty :as jetty]))
+
+(defn json-response [data & [status]]
+    {:status (or status 200)
+        :headers {"Content-Type" "application/json"}
+        :body (json/write-str data)})
+
+(def counter (atom 1000 ))
+(def urls (atom {}))
+
+(defn shorten
+  [url]
+  (let [id (swap! counter inc)
+        id (Long/toString id 36)]
+       (swap! urls assoc id url)
+    id))
+
+(defn redirect
+  [id]
+  (@urls id))
+
+(defroutes handler 
+  (POST "/"   [url] (json-response (shorten url )))
+  (GET "/:id" [id]  (json-response (redirect id ))))
+
+(def app
+  (-> handler
+    wrap-json-params))
+
+(defn -main []
+  (jetty/run-jetty #'app {:port 8080 :join? false}))
