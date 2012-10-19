48fcc0f94fd49d337d58611611cef13a5a243f0f
nakort <nakort@gmail.com>
2012-10-19 10:25:06 -0430

    Set up linkshortener project in clojure

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject linkshortener "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0" ]
+                 [ring/ring-json "0.1.2" ]
+                 [org.clojure/data.json "0.2.0"]
+                 [ring "1.1.6" ]
+                 [compojure "1.1.3"]
+                 [enlive "1.0.0"]]) 
